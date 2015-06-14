5bc0a846278ca393439e0da9aabb8d7cab27cee3
Zachary Meadows <zac@Zacharys-MacBook-Pro.local>
2015-06-14 16:47:09 -0400

    root-finding now working

--- Makefile
+++
@@ -1,2 +0,0 @@
-bloch:
-	clang bloch.c -L/usr/local/Cellar/sundials/2.5.0/lib -lsundials_cvode -lsundials_nvecserial -lm -o bloch -O3
--- bloch.c
+++ bloch.c
@@ -66,17 +66,14 @@ struct bloch_sim {
     realtype fid_sampling_interval;
     realtype fid_sampling_rate;
 
-    int pi2_num_samples;
-    int pi2_samples_per_period;
     realtype pi2_duration;
-    realtype pi2_sampling_interval;
-    realtype pi2_sampling_rate;
 
+    int estimated_num_zero_crossings;
     int num_zero_crossings;
     realtype *zero_crossings;
 };
 
-static void initialize_bloch(struct bloch_sim *bsp, realtype _p_0, realtype _p_1);
+static void initialize_bloch(struct bloch_sim *bs, realtype _p_0, realtype _p_1);
 
 static int bloch_equations(realtype t, N_Vector M, N_Vector Mdot, void *user_data);
 
@@ -86,53 +83,53 @@ static int bloch_jacobian(long int N, realtype t,
 
 static int bloch_root(realtype t, N_Vector y, realtype *gout, void *user_data);
 
-static void initialize_bloch(struct bloch_sim *bsp, realtype _p_0, realtype _p_1)
+// static void print_diagnostics(struct bloch_sim *bs);
+// static void print_frequencies(struct bloch_sim *bs);
+
+
+static void initialize_bloch(struct bloch_sim *bs, realtype _p_0, realtype _p_1)
 {
-    bsp->w_0 = 2*M_PI*60e3;
-    bsp->B_0 = bsp->w_0 / GAMMA_P;
-    bsp->w_1 = -0.1 * bsp->w_0;
-    bsp->B_1 = -bsp->w_1 / GAMMA_P;
+    bs->w_0 = 2*M_PI*60e6;
+    bs->B_0 = bs->w_0 / GAMMA_P;
+    bs->w_1 = -0.1 * bs->w_0;
+    bs->B_1 = -bs->w_1 / GAMMA_P;
 
-    bsp-> T_1 = 5e-3;
-    bsp-> T_2 = 5e-3;
+    bs-> T_1 = 5e-3;
+    bs-> T_2 = 5e-3;
 
-    bsp->p_0 = _p_0;
-    bsp->p_1 = _p_1;
+    bs->p_0 = _p_0;
+    bs->p_1 = _p_1;
 
-    bsp->num_cells = 3;
-    bsp->cell_frequencies = malloc(bsp->num_cells * sizeof(realtype));
-    bsp->cell_positions   = malloc(bsp->num_cells * sizeof(realtype));
+    bs->num_cells = 3;
+    bs->cell_frequencies = malloc(bs->num_cells * sizeof(realtype));
+    bs->cell_positions   = malloc(bs->num_cells * sizeof(realtype));
 
     int i; realtype x; realtype w_cell; realtype w_total;
     w_total = 0;
-    for (i=0; i<bsp->num_cells; ++i)
+    for (i=0; i<bs->num_cells; ++i)
     {
-        x = -1.0 + i*2.0/(bsp->num_cells - 1);
-        bsp->cell_positions[i] = x;
+        x = -1.0 + i*2.0/(bs->num_cells - 1);
+        bs->cell_positions[i] = x;
 
-        w_cell = bsp->w_0 + bsp->p_0*bsp->w_0*x + bsp->p_1*bsp->w_0*x*x;
-        bsp->cell_frequencies[i] = w_cell;
+        w_cell = bs->w_0 + bs->p_0*bs->w_0*x + bs->p_1*bs->w_0*x*x;
+        bs->cell_frequencies[i] = w_cell;
         w_total += w_cell;
     }
 
-    bsp->w_avg = w_total / bsp->num_cells;
-    bsp->B_avg = bsp->w_avg / GAMMA_P;
+    bs->w_avg = w_total / bs->num_cells;
+    bs->B_avg = bs->w_avg / GAMMA_P;
 
-    //TODO: increase pi2 samples per period to max necessary
-    bsp->pi2_samples_per_period = 45;
-    bsp->pi2_duration           = -M_PI / (2*bsp->w_1);
-    bsp->pi2_num_samples        = bsp->pi2_samples_per_period * (int)(bsp->pi2_duration * bsp->w_avg / (2.0*M_PI));
-    bsp->pi2_sampling_rate      = bsp->pi2_num_samples / bsp->pi2_duration;
-    bsp->pi2_sampling_interval  = bsp->pi2_duration / bsp->pi2_num_samples;
+    bs->pi2_duration           = -M_PI / (2*bs->w_1);
 
-    bsp->fid_samples_per_period = 15;
-    bsp->fid_duration           = bsp->T_2 * 6;
-    bsp->fid_num_samples        = bsp->fid_samples_per_period * bsp->fid_duration * bsp->w_avg / (2*M_PI);
-    bsp->fid_sampling_rate      = bsp->fid_num_samples / bsp->fid_duration;
-    bsp->fid_sampling_interval  = bsp->fid_duration / bsp->fid_num_samples;
+    bs->fid_samples_per_period = 45;
+    bs->fid_duration           = bs->T_2 * 6;
+    bs->fid_num_samples        = bs->fid_samples_per_period * bs->fid_duration * bs->w_avg / (2*M_PI);
+    bs->fid_sampling_rate      = bs->fid_num_samples / bs->fid_duration;
+    bs->fid_sampling_interval  = bs->fid_duration / bs->fid_num_samples;
 
-    bsp->num_zero_crossings = 0;
-    bsp->zero_crossings = malloc( (int)( bsp->fid_duration / (M_PI / bsp->w_avg) + 1000 ) ); // add extra 1000 just to be sure the array is long enough.
+    bs->num_zero_crossings = 0;
+    bs->estimated_num_zero_crossings = (int)( bs->fid_duration / (M_PI / bs->w_avg));
+    bs->zero_crossings = malloc( sizeof(realtype) * bs->estimated_num_zero_crossings * 2 );
 
     // {{{ DEBUG BLOCH PARAMS PRINT STATEMENTS
     if (DEBUG)
@@ -141,60 +138,56 @@ static void initialize_bloch(struct bloch_sim *bsp, realtype _p_0, realtype _p_1
         printf("### BLOCH SIMULATION PARAMETERS ###\n");
         printf("###################################\n");
 
-        printf("w_0: %.5e (rad/sec), %.5e (Hz)\n", bsp->w_0, bsp->w_0 / (2*M_PI));
-        printf("B_0: %.5e (Tesla)\n", bsp->B_0);
-        printf("w_1: %.5e (rad/sec), %.5e (Hz)\n", bsp->w_1, bsp->w_1 / (2*M_PI));
-        printf("B_1: %.5e (Tesla)\n", bsp->B_1);
-        printf("T_1: %.5e (sec)\n", bsp->T_1);
-        printf("T_2: %.5e (sec)\n", bsp->T_2);
-        printf("p_0: %.5e (linear perturbation coefficient)\n", bsp->p_0);
-        printf("p_1: %.5e (quadratic perturbation coefficient)\n", bsp->p_1);
+        printf("w_0: %.5e (rad/sec), %.5e (Hz)\n", bs->w_0, bs->w_0 / (2*M_PI));
+        printf("B_0: %.5e (Tesla)\n", bs->B_0);
+        printf("w_1: %.5e (rad/sec), %.5e (Hz)\n", bs->w_1, bs->w_1 / (2*M_PI));
+        printf("B_1: %.5e (Tesla)\n", bs->B_1);
+        printf("T_1: %.5e (sec)\n", bs->T_1);
+        printf("T_2: %.5e (sec)\n", bs->T_2);
+        printf("p_0: %.5e (linear perturbation coefficient)\n", bs->p_0);
+        printf("p_1: %.5e (quadratic perturbation coefficient)\n", bs->p_1);
         printf("\n");
         printf("#############\n");
         printf("### CELLS ###\n");
         printf("#############\n");
-        printf("num_cells: %d\n\n", bsp->num_cells);
+        printf("num_cells: %d\n\n", bs->num_cells);
         int i;
-        for (i=0; i<bsp->num_cells; ++i)
+        for (i=0; i<bs->num_cells; ++i)
         {
-            printf("cell %d position: %.5e\n", i, bsp->cell_positions[i]);
-            printf("cell %d larmor frequency: %.5e (rad/sec), %.5e (Hz)\n", i, bsp->cell_frequencies[i], bsp->cell_frequencies[i] / (2*M_PI));
+            printf("cell %d position: %.5e\n", i, bs->cell_positions[i]);
+            printf("cell %d larmor frequency: %.5e (rad/sec), %.5e (Hz)\n", i, bs->cell_frequencies[i], bs->cell_frequencies[i] / (2*M_PI));
             printf("\n");
         }
 
-        printf("w_avg: %.5e (rad/sec), %.5e (Hz)\n", bsp->w_avg, bsp->w_avg / (2*M_PI));
-        printf("B_avg: %.5e (Tesla)\n", bsp->B_avg);
+        printf("w_avg: %.5e (rad/sec), %.5e (Hz)\n", bs->w_avg, bs->w_avg / (2*M_PI));
+        printf("B_avg: %.5e (Tesla)\n", bs->B_avg);
 
         printf("\n");
         printf("###############################\n");
         printf("### FID SAMPLING PROPERTIES ###\n");
         printf("###############################\n");
 
-        printf("fid_num_samples: %d\n", bsp->fid_num_samples);
-        printf("fid_samples_per_period: %d\n", bsp->fid_samples_per_period);
-        printf("fid_duration: %.5e (sec)\n", bsp->fid_duration);
-        printf("fid_sampling_interval: %.5e (sec)\n", bsp->fid_sampling_interval);
-        printf("fid_sampling_rate: %.5e (Hz)\n", bsp->fid_sampling_rate);
+        printf("fid_num_samples: %d\n", bs->fid_num_samples);
+        printf("fid_samples_per_period: %d\n", bs->fid_samples_per_period);
+        printf("fid_duration: %.5e (sec)\n", bs->fid_duration);
+        printf("fid_sampling_interval: %.5e (sec)\n", bs->fid_sampling_interval);
+        printf("fid_sampling_rate: %.5e (Hz)\n", bs->fid_sampling_rate);
 
         printf("\n");
         printf("######################################\n");
         printf("### PI/2 PULSE SAMPLING PROPERTIES ###\n");
         printf("######################################\n");
 
-        printf("pi2_num_samples: %d\n", bsp->pi2_num_samples);
-        printf("pi2_samples_per_period: %d\n", bsp->pi2_samples_per_period);
-        printf("pi2_duration: %.5e (sec)\n", bsp->pi2_duration);
-        printf("pi2_sampling_interval: %.5e (sec)\n", bsp->pi2_sampling_interval);
-        printf("pi2_sampling_rate: %.5e (Hz)\n", bsp->pi2_sampling_rate);
+        printf("pi2_duration: %.5e (sec)\n", bs->pi2_duration);
     }
     // }}}
 }
 
-static void free_bloch(struct bloch_sim *bsp)
+static void free_bloch(struct bloch_sim *bs)
 {
-    free(bsp->cell_positions);
-    free(bsp->cell_frequencies);
-    free(bsp);
+    free(bs->cell_positions);
+    free(bs->cell_frequencies);
+    free(bs);
 }
 
 // {{{ CHECK FLAG FUNCTION
@@ -226,25 +219,25 @@ static int check_flag(void *flagvalue, char *funcname, int opt)
 }
 // }}}
 
-static int simulate_nmr_pulse(struct bloch_sim *bsp)
+static int simulate_nmr_pulse(struct bloch_sim *bs)
 {
     N_Vector M = NULL;
-    M = N_VNew_Serial(3 * bsp->num_cells);
+    M = N_VNew_Serial(3 * bs->num_cells);
     if (check_flag((void *)M, "N_VNew_Serial", 0)) return(1);
 
     int i;
     /* Set initial (t=0) magnetization conditions */
-    for(i=0; i < bsp->num_cells; ++i)
+    for(i=0; i < bs->num_cells; ++i)
     {
         X(M,i) = 0.0;
         Y(M,i) = 0.0;
-        Z(M,i) = bsp->cell_frequencies[i] / bsp->w_avg;
+        Z(M,i) = bs->cell_frequencies[i] / bs->w_avg;
     }
 
-    realtype reltol = RCONST(1.0e-13);
-    realtype abstol = RCONST(1.0e-13);
+    realtype reltol = RCONST(1.0e-14);
+    realtype abstol = RCONST(1.0e-14);
 
-    void *cvode_mem = CVodeCreate(CV_BDF, CV_NEWTON);
+    void *cvode_mem = CVodeCreate(CV_ADAMS, CV_FUNCTIONAL);
     if (check_flag((void *)cvode_mem, "CVodeCreate", 0)) return(1);
 
     int flag;
@@ -252,13 +245,13 @@ static int simulate_nmr_pulse(struct bloch_sim *bsp)
     flag = CVodeInit(cvode_mem, bloch_equations, 0.0, M);
     if (check_flag(&flag, "CVodeInit", 1)) return(1);
 
-    flag = CVodeSetUserData(cvode_mem, bsp);
+    flag = CVodeSetUserData(cvode_mem, bs);
     if (check_flag(&flag, "CVodeSetUserData", 1)) return(1);
 
     flag = CVodeSStolerances(cvode_mem, reltol, abstol);
     if (check_flag(&flag, "CVodeSetUserData", 1)) return(1);
 
-    flag = CVDense(cvode_mem, 3 * bsp->num_cells);
+    flag = CVDense(cvode_mem, 3 * bs->num_cells);
     if (check_flag(&flag, "CVDense", 1)) return(1);
 
     flag = CVDlsSetDenseJacFn(cvode_mem, bloch_jacobian);
@@ -267,13 +260,13 @@ static int simulate_nmr_pulse(struct bloch_sim *bsp)
     ///////////////////////////
     // PI/2 PULSE SIMULATION //
     ///////////////////////////
-    bsp->rf_on = 1;
+    bs->rf_on = 1;
 
-    flag = CVodeSetStopTime(cvode_mem, bsp->pi2_duration);
+    flag = CVodeSetStopTime(cvode_mem, bs->pi2_duration);
     if (check_flag(&flag, "CVodeSetStopTime", 1)) return 1;
 
     realtype time_reached;
-    flag = CVode(cvode_mem, bsp->pi2_duration, M, &time_reached, CV_NORMAL);
+    flag = CVode(cvode_mem, bs->pi2_duration, M, &time_reached, CV_NORMAL);
 
     if (flag != CV_SUCCESS)
     {
@@ -290,11 +283,11 @@ static int simulate_nmr_pulse(struct bloch_sim *bsp)
         printf("\n");
 
         printf("TIME REACHED: %.15e\n", time_reached);
-        printf("TIME REACHED - PI2_DURATION: %.15e\n", time_reached - bsp->pi2_duration);
+        printf("TIME REACHED - PI2_DURATION: %.15e\n", time_reached - bs->pi2_duration);
 
         printf("\n");
         printf("MAGNETIZATION AT END OF PI/2 PULSE:\n");
-        for (i = 0; i<bsp->num_cells; ++i)
+        for (i = 0; i<bs->num_cells; ++i)
         {
             printf("CELL %d: %.4e, %.4e, %.4e\n", i, X(M,i), Y(M,i), Z(M,i));
         }
@@ -304,7 +297,7 @@ static int simulate_nmr_pulse(struct bloch_sim *bsp)
     ////////////////////
     // FID SIMULATION //
     ////////////////////
-    bsp->rf_on = 0;
+    bs->rf_on = 0;
 
     if (DEBUG)
     {
@@ -317,42 +310,29 @@ static int simulate_nmr_pulse(struct bloch_sim *bsp)
     if (check_flag(&flag, "CVodeReInit", 1)) return(1);
     time_reached = 0.0;
 
-    flag = CVodeSetStopTime(cvode_mem, bsp->fid_duration);
+    flag = CVodeSetStopTime(cvode_mem, bs->fid_duration);
     if (check_flag(&flag, "CVodeSetStopTime", 1)) return 1;
 
     flag = CVodeRootInit(cvode_mem, 1, bloch_root);
     if (check_flag(&flag, "CVodeRootInit", 1)) return 1;
 
-    realtype time_desired, M_FID_LAB;
-    while (time_reached < bsp->fid_duration)
+    realtype time_desired;
+    while (time_reached < bs->fid_duration)
     {
-        if (flag == CV_SUCCESS && WRITE_FID)
-        {
-            M_FID_LAB = 0.0;
-            for (i=0; i < bsp->num_cells; ++i)
-            {
-                M_FID_LAB -= X(M,i) * sin(bsp->w_avg * time_reached);
-                M_FID_LAB += Y(M,i) * cos(bsp->w_avg * time_reached);
-            }
-            M_FID_LAB /= bsp->num_cells;
-
-            // write fid samples to file here
-        }
-
-        time_desired = time_reached + bsp->fid_sampling_interval;
+        time_desired = time_reached + bs->fid_sampling_interval;
         flag = CVode(cvode_mem, time_desired, M, &time_reached, CV_NORMAL);
 
         if (flag == CV_ROOT_RETURN)
         {
-            bsp->zero_crossings[bsp->num_zero_crossings] = time_reached;
-            bsp->num_zero_crossings++;
+            bs->zero_crossings[bs->num_zero_crossings] = time_reached;
+            bs->num_zero_crossings++;
         }
-
     }
 
-    if (WRITE_FID_FREQUENCIES)
-    {
-
+    bs->zero_crossings = (realtype*) realloc(bs->zero_crossings, sizeof(realtype) * bs->num_zero_crossings);
+    if (!bs->zero_crossings) {
+        printf("ERROR: reallocating zero crossing array memory failed!\n");
+        exit(1);
     }
 
     N_VDestroy_Serial(M);
@@ -364,20 +344,20 @@ static int simulate_nmr_pulse(struct bloch_sim *bsp)
 
 static int bloch_equations(realtype t, N_Vector M, N_Vector Mdot, void *user_data)
 {
-    struct bloch_sim *bsp = (struct bloch_sim*)user_data;
+    struct bloch_sim *bs = (struct bloch_sim*)user_data;
 
     int i;
     realtype dw, w_1, w_cell, Mx, My, Mz;
-    for (i=0; i < bsp->num_cells; ++i)
+    for (i=0; i < bs->num_cells; ++i)
     {
-        w_cell = bsp->cell_frequencies[i];
-        dw =  w_cell - bsp->w_avg;
-        w_1 = bsp->rf_on ? bsp->w_1 : 0.0;
+        w_cell = bs->cell_frequencies[i];
+        dw =  w_cell - bs->w_avg;
+        w_1 = bs->rf_on ? bs->w_1 : 0.0;
         Mx = X(M,i); My = Y(M,i); Mz = Z(M,i);
 
-        X(Mdot,i) = dw * My - Mx / bsp->T_2;
-        Y(Mdot,i) = -dw * Mx + w_1 * Mz - My / bsp->T_2;
-        Z(Mdot,i) = -w_1 * My + (w_cell/bsp->w_avg - Mz) / bsp->T_1;
+        X(Mdot,i) = dw * My - Mx / bs->T_2;
+        Y(Mdot,i) = -dw * Mx + w_1 * Mz - My / bs->T_2;
+        Z(Mdot,i) = -w_1 * My + (w_cell/bs->w_avg - Mz) / bs->T_1;
     }
 
     return 0;
@@ -387,23 +367,23 @@ static int bloch_jacobian(long int N, realtype t,
         N_Vector M, N_Vector fM, DlsMat J, void *user_data,
         N_Vector tmp1, N_Vector tmp2, N_Vector tmp3)
 {
-    struct bloch_sim *bsp = (struct bloch_sim*)user_data;
+    struct bloch_sim *bs = (struct bloch_sim*)user_data;
 
     int i;
-    for (i=0; i < bsp->num_cells; ++i)
+    for (i=0; i < bs->num_cells; ++i)
     {
-        realtype dw = bsp->cell_frequencies[i] - bsp->w_avg;
-        realtype w_1 = bsp->rf_on ? bsp->w_1 : 0.0;
+        realtype dw = bs->cell_frequencies[i] - bs->w_avg;
+        realtype w_1 = bs->rf_on ? bs->w_1 : 0.0;
 
-        XX(J,i) = -1 / bsp->T_2;
+        XX(J,i) = -1 / bs->T_2;
         XY(J,i) = dw;
         XZ(J,i) = 0.0;
         YX(J,i) = -dw;
-        YY(J,i) = -1 / bsp->T_2;
+        YY(J,i) = -1 / bs->T_2;
         YZ(J,i) = w_1;
         ZX(J,i) = 0.0;
         ZY(J,i) = -w_1;
-        ZZ(J,i) = -1 / bsp->T_1;
+        ZZ(J,i) = -1 / bs->T_1;
     }
 
     return 0;
@@ -411,20 +391,19 @@ static int bloch_jacobian(long int N, realtype t,
 
 static int bloch_root(realtype t, N_Vector M, realtype *gout, void *user_data)
 {
-    struct bloch_sim *bsp = (struct bloch_sim*)user_data;
+    struct bloch_sim *bs = (struct bloch_sim*)user_data;
 
     gout[0] = 0.0;
     int i;
-    for (i=0; i < bsp->num_cells; ++i)
+    for (i=0; i < bs->num_cells; ++i)
     {
-        gout[0] -= X(M,i) * sin(bsp->w_avg * t);
-        gout[0] += Y(M,i) * cos(bsp->w_avg * t);
+        gout[0] -= X(M,i) * sin(bs->w_avg * t);
+        gout[0] += Y(M,i) * cos(bs->w_avg * t);
     }
 
     return 0;
 }
 
-// static void calculate_frequencies(realtype *zero_crossings, int , realtype *frequencies)
 // {
 // }
 
@@ -432,6 +411,43 @@ int
 main (int argc, char **argv)
 {
     struct bloch_sim *b = malloc(sizeof(*b));
+
+    int option = 0;
+
+    realtype p_0 = 0.0;
+    realtype p_1 = 0.0;
+
+    FILE *output_file;
+
+    while ((option = getopt(argc, argv,"l:q:o:")) != -1)
+    {
+        switch (option)
+        {
+            case 'l' :
+                p_0 = strtod(optarg, NULL);
+                break;
+
+            case 'q' :
+                p_1 = strtod(optarg, NULL);
+                break;
+
+            case 'o' :
+                output_file = fopen(optarg, "w");
+                if (output_file == NULL) {
+                    fprintf(stderr, "ERROR: failed to open output file!\n");
+                    exit(1);
+                }
+
+            default:
+                //print_usage();
+                break;
+        }
+    }
+
+
+    if (output_file != NULL)
+    {
+    }
 
     initialize_bloch(b, 0.0, 1e-3);
     simulate_nmr_pulse(b);
